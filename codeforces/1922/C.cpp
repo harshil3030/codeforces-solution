655b763d55cb21e036c7689c274511b21a5e6210
harshil <savaliyaharshil3030@gmail.com>
2024-01-28 19:51:00 +0530

    Add solution for problem `C - Closest Cities` Link: https://codeforces.com/contest/1922/problem/C Tags: greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1922/submission/243782072

--- codeforces/1922/C.cpp
+++ codeforces/1922/C.cpp
@@ -1,11 +1,3 @@
-// Problem: C. Closest Cities
-// Contest: Codeforces - Educational Codeforces Round 161 (Rated for Div. 2)
-// URL: https://codeforces.com/contest/1922/problem/C
-// Memory Limit: 256 MB
-// Time Limit: 2000 ms
-// 
-// Powered by CP Editor (https://cpeditor.org)
-
 #include<bits/stdc++.h>
 using namespace std;
 long long t = 0;
